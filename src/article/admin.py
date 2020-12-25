dcfe09f7e2dbc5ffc1244f0434e31bbc92cd1768
yusuf <ysfozturk3@gmail.com>
2020-12-25 22:22:25 +0100

    created an app and model

---
+++ src/article/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Article
+
+# Register your models here.
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title','author','created_date',]
+    list_display_links = ['title']
+    search_fields = ['title']
+    list_filter= ['created_date']
+    class Meta:
+        model = Article
+
