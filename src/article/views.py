8361695bc75f9176ffb253cdc58bdf5cb34aa30c
yusuf <ysfozturk3@gmail.com>
2020-12-28 20:02:39 +0100

    main page header

--- src/article/views.py
+++ src/article/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render
+
 
 # Create your views here.
 
 def index(request):
-   return render(request,'home.html')
+   return render(request,'index.html')
