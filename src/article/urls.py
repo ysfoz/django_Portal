85a3e1185670a9fb88e1935f28c83289c40bb6ab
yusuf <ysfozturk3@gmail.com>
2020-12-28 22:12:18 +0100

    constructed urls

--- src/article/urls.py
+++ src/article/urls.py
@@ -3,6 +3,6 @@ from .views import index,about
 
 urlpatterns = [
    
-    path('', index,name='index'),
-    path('about/', about,name='about'),
+    path('create/', index,name='index'),
+  
 ]
--- src/medium/urls.py
+++ src/medium/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from article.views import index , about
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('article.urls')),
+    path('', index,name='index'),
+    path('about/', about,name='about'),
+    path('articles/', include('article.urls')),
 ]
+
