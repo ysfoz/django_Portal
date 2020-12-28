1e97e4e1370c8ae8582bf35a414dd549a7e7d177
yusuf <ysfozturk3@gmail.com>
2020-12-28 21:03:30 +0100

    added about page

--- src/article/urls.py
+++ src/article/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index,about
 
 urlpatterns = [
    
-    path('', index,name='index')
+    path('', index,name='index'),
+    path('about/', about,name='about'),
 ]
--- src/article/views.py
+++ src/article/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 
 def index(request):
    return render(request,'index.html')
+
+def about(request):
+   return render(request, 'about.html')
