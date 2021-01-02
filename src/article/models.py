e168bc402311b52950b6ef49c66cdfc118765de6
yusuf <ysfozturk3@gmail.com>
2021-01-02 14:56:26 +0100

    created comment model

--- src/article/models.py
+++ src/article/models.py
@@ -11,3 +11,11 @@ class Article(models.Model):
     def __str__(self):
         return self.title
     
+class Comment(models.Model):
+    article=models.ForeignKey(Article,on_delete=models.CASCADE,related_name='comments')
+    comment_author = models.CharField(max_length=50)
+    comment_content = models.CharField(max_length=200)
+    comment_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.comment_content
